a962456948cbfca7168a49c356a3e62a6fb4acf4
sayem499 <sayem499@gmail.com>
2024-10-15 21:49:28 +0600

    Build with out

--- outlet-landing-page/next.config.mjs
+++ outlet-landing-page/next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
         // This option allows production builds to successfully complete even if your project has ESLint errors.
         ignoreDuringBuilds: true,
       },
+    output: 'export',
+    trailingSlash: true,
+    skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
