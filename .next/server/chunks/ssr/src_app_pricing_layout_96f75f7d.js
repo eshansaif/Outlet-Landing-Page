934ff43d74bde8906ebaba9f6d86d5ed7bf292ef
sayem499 <sayem499@gmail.com>
2025-11-24 15:14:58 +0600

    Changed structure

---
+++ .next/server/chunks/ssr/src_app_pricing_layout_96f75f7d.js
@@ -0,0 +1,3 @@
+module.exports=[28842,a=>{"use strict";var b=a.i(7997);function c({children:a}){return(0,b.jsx)("div",{children:(0,b.jsxs)("main",{className:"h-full w-full",children:[a,"  "]})})}a.s(["default",()=>c])}];
+
+//# sourceMappingURL=src_app_pricing_layout_96f75f7d.js.map
---
+++ .next/server/chunks/ssr/src_app_privacy_layout_cff83598.js
@@ -0,0 +1,3 @@
+module.exports=[73355,a=>{"use strict";var b=a.i(7997);function c({children:a}){return(0,b.jsx)("div",{children:(0,b.jsxs)("main",{className:"h-full w-full",children:[a,"  "]})})}a.s(["default",()=>c])}];
+
+//# sourceMappingURL=src_app_privacy_layout_cff83598.js.map
